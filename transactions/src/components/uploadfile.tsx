cb1ccc4b626f3f8feba9a6cd5bdd4735c7400230
Adriano Bono <adriano@radek.com.br>
2022-04-17 15:49:53 -0300

    update parse data

--- transactions/src/components/uploadfile.tsx
+++ transactions/src/components/uploadfile.tsx
@@ -1,45 +1,50 @@
-import React from 'react'
-import { parseISO } from 'date-fns'
+import React from "react";
 
 export default function uploadfile() {
-//const ruleparser =[1,9,19,30,42,48,62,81]
-const ruleparser =[1,10,21,33,46,53,68,90]
-
-
+  const ruleparser = [1, 10, 21, 33, 46, 53, 68, 89];
 
   return (
     <div>
-      <input type="file" onChange={(e:React.ChangeEvent<HTMLInputElement>): void => {
-        e.preventDefault()
-       if(e.target.files?.length){
-        const reader = new FileReader()
-        reader.onload = async (e:any) => { 
-          const text = (e.target.result);
-          let parseData:any = JSON.stringify(text.toString().split("\n"));
-          
-          parseData = JSON.parse(parseData);
-          parseData.pop();
-        
-          if(parseData.length > 0) {
-             parseData.map((item:any) =>{
-                ruleparser.map((indice:number)=>{
-                
-                 item = [item.slice(0, indice).trim(),   item.slice(indice).trim()].join(';');
-                 
-                }) 
-                console.log(item)
-             })
+      <input
+        type="file"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>): void => {
+          e.preventDefault();
+          if (e.target.files?.length) {
+            const reader = new FileReader();
+            reader.onload = async (e: any) => {
+              const text = e.target.result;
+              let rawData: any = JSON.stringify(text.toString().split("\n"));
+              let parseData: any = [];
+
+              rawData = JSON.parse(rawData);
+              rawData.pop();
+
+            rawData.length > 0 &&
+                rawData.forEach((item: string) => {
+                  ruleparser.forEach((indice: number) => {
+                    item = [
+                      item.slice(0, indice).trim(),
+                      item.slice(indice).trim(),
+                    ].join(indice < 80 ? ";" : "");
+                  });
+                  let tempItem: Array<any>
+                  tempItem = item.split(";");
+                  tempItem[2] = Number(tempItem[2]) / 100;
+                  tempItem[1] = new Date(
+                    parseInt(tempItem[1].substr(0, 4)),
+                    parseInt(tempItem[1].substr(4, 2)) - 1,
+                    parseInt(tempItem[1].substr(6, 2)),
+                    parseInt(tempItem[5].substr(0, 2)),
+                    parseInt(tempItem[5].substr(2, 4)),
+                    parseInt(tempItem[5].substr(4, 6))
+                  ).toISOString();
+                  parseData.push(item);
+                });           
+            };
+            reader.readAsText(e.target.files[0]);
           }
-        
-        };
-        reader.readAsText(e.target.files[0])
-
-
-        // console.log(e.target.files[0]);
-        console.log(JSON.stringify(e.target.files[0]))
-         
-       }
-      }}/>
+        }}
+      />
     </div>
-  )
+  );
 }
